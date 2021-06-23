6313fc0685aa063b9eb19b5231281ad11134db7b
Lucio Palmieri <lpalmieri@promoqui.it>
2021-06-23 19:39:03 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ae83f2ea20e95a9c916a.js"
+    "url": "webpack-runtime-79a004f26e56e1568138.js"
   },
   {
     "url": "framework-d906ef765e04967ce4a2.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ea727277388ae2d9ec618ce9baf3c215"
+    "revision": "d56150fba4748b67069b758d53cd1c5b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5eb99d690adca45fa441.js"
